d8a56eae7f5195e4dc8ef20ac1f094b96e23e61e
dhax <dhax@users.noreply.github.com>
2019-01-12 15:53:47 +0100

    adds client demo for heroku deploy

---
+++ public/precache-manifest.0f2df6871fb6a0e7dadad1c88b247db2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f22e476c568802b535b",
+    "url": "/js/about.87b858b5.js"
+  },
+  {
+    "revision": "832105b1d08668b99e3d",
+    "url": "/css/account.37c464f9.css"
+  },
+  {
+    "revision": "832105b1d08668b99e3d",
+    "url": "/js/account.ab92099b.js"
+  },
+  {
+    "revision": "44615f3eca25bdd0226d",
+    "url": "/js/admin.7d1f7d59.js"
+  },
+  {
+    "revision": "aa66b4e1e1c196104194",
+    "url": "/css/app.afa773e4.css"
+  },
+  {
+    "revision": "aa66b4e1e1c196104194",
+    "url": "/js/app.62629129.js"
+  },
+  {
+    "revision": "33823c7a459d35fef426",
+    "url": "/css/chunk-69f8d7c8.ddb698de.css"
+  },
+  {
+    "revision": "33823c7a459d35fef426",
+    "url": "/js/chunk-69f8d7c8.64905801.js"
+  },
+  {
+    "revision": "6b62bfe8cc85a889bd95",
+    "url": "/css/chunk-6fc85424.a19fdb43.css"
+  },
+  {
+    "revision": "6b62bfe8cc85a889bd95",
+    "url": "/js/chunk-6fc85424.541c54e1.js"
+  },
+  {
+    "revision": "984fc3521aecc1557ee9",
+    "url": "/css/chunk-vendors.27787cc8.css"
+  },
+  {
+    "revision": "984fc3521aecc1557ee9",
+    "url": "/js/chunk-vendors.99217085.js"
+  },
+  {
+    "revision": "2af3315f0a43de802f30daec241e391b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
